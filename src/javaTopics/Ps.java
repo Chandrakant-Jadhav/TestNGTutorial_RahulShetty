f7773efb63ceb1d5803d62c6ac4ea8fa92487961
Chandrakant <chandrakantjadhav.qa@gmail.com>
2026-01-28 23:48:28 +0530

    Java - Inheritance & TestNG

--- src/javaTopics/Ps.java
+++ src/javaTopics/Ps.java
@@ -1,9 +1,25 @@
 package javaTopics;
 
+//import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+
 public class Ps {
 	
 	public void doThis() {
 		System.out.println("Doing this");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		System.out.println("I will execute before every method in Ps1 class");
+	}
+	
+	
+	@AfterMethod
+	public void AfterMethod()
+	{
+		System.out.println("I will execute After all method in Ps1 class");
 	}
 
 }
--- src/javaTopics/Ps1.java
+++ src/javaTopics/Ps1.java
@@ -1,13 +1,21 @@
 package javaTopics;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Ps1 extends Ps {
 
 	
-		@Test
+	@Test
 		public void FirstTest() {
-			doThis();
+		
+		Ps2 ps2=new Ps2();
+		
+		
+		int a=3;	
+		doThis();
+		
+		System.out.println(ps2.increment());
 	}
 
 }
